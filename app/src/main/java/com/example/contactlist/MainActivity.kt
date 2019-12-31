3027c11d729ff4355df28a5b898f6a3a507a75b9
Dubovec <dubovec95@ukr.net>
2019-12-31 21:47:51 +0200

    Adding Database

--- app/src/main/java/com/example/contactlist/MainActivity.kt
+++ app/src/main/java/com/example/contactlist/MainActivity.kt
@@ -3,30 +3,37 @@ package com.example.contactlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.sqlitesimplekotlin.DatabaseHelper
 import kotlinx.android.synthetic.main.main_layout.*
 import kotlinx.android.synthetic.main.new_contact.*
 import kotlinx.android.synthetic.main.new_contact.view.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private var databaseHelper: DatabaseHelper? = null
+    private var arrayList: ArrayList<String>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
 
+
         //button click to show dialog
         mainLoginBtn.setOnClickListener {
-
 
             //Inflate the dialog with custom view
             val mDialogView = LayoutInflater.from(this).inflate(R.layout.new_contact, null)
             //AlertDialogBuilder
             val mBuilder = AlertDialog.Builder(this)
                 .setView(mDialogView)
-                .setTitle("Login Form")
+                .setTitle("Adding new contact")
             //show dialog
-            val  mAlertDialog = mBuilder.show()
+            val mAlertDialog = mBuilder.show()
             //login button click of custom layout
             mDialogView.addContact.setOnClickListener {
                 val firstName = mDialogView.firstName.text.toString()
@@ -36,14 +43,17 @@ class MainActivity : AppCompatActivity() {
                 //dismiss dialog
                 if (firstName.isEmpty()) {
                     Toast.makeText(this, "Enter First Name!", Toast.LENGTH_SHORT).show()
-                } else if (homeNumber.isEmpty()){
+                } else if (homeNumber.isEmpty()) {
                     Toast.makeText(this, "Enter Home number!", Toast.LENGTH_SHORT).show()
                 } else {
                     mAlertDialog.dismiss()
                     //get text from EditTexts of custom layout
 
                     //set the input text in TextView
-                    mainInfoTv.setText(firstName + "\n" + lastName + "\n " + homeNumber + "\n" + workNumber)
+//                    mainInfoTv.setText(firstName + "\n" + lastName + "\n " + homeNumber + "\n" + workNumber)
+                    databaseHelper!!.addStudentDetail(firstName, lastName, homeNumber, workNumber)
+
+                    Toast.makeText(this, "Stored Successfully!", Toast.LENGTH_SHORT).show()
                 }
             }
 
@@ -54,8 +64,17 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+        databaseHelper = DatabaseHelper(this)
+
+
+        show.setOnClickListener {
+            arrayList = databaseHelper!!.allContactsList
+            mainInfoTv!!.text = ""
+            mainInfoTv!!.text = arrayList.toString()
+
+
+
+        }
     }
-
-
-
 }
